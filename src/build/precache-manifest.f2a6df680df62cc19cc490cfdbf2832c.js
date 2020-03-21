2be030ba1afa32b0337ead65bff5b89683b193e8
Mike Camara <mcmikecamara@gmail.com>
2020-03-21 20:12:19 +0200

    Create blog server

---
+++ src/build/precache-manifest.f2a6df680df62cc19cc490cfdbf2832c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff28750f2d7fdc991cf8962f370ed4da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b36977f3569872cdac87",
+    "url": "/static/css/main.82de17af.chunk.css"
+  },
+  {
+    "revision": "164a506fe1fc207e8f6d",
+    "url": "/static/js/2.648bb578.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.648bb578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36977f3569872cdac87",
+    "url": "/static/js/main.8b009321.chunk.js"
+  },
+  {
+    "revision": "f03f2ec75f1ea6533a62",
+    "url": "/static/js/runtime-main.9f322348.js"
+  }
+]);
